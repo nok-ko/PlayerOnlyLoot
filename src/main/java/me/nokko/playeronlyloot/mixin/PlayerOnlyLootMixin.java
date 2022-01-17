d2f786e71d5dbe4700e447813f5be18bc1d66978
nokko <13428215+nok-ko@users.noreply.github.com>
2022-01-16 16:38:35 -0800

    Backport to 1.16.5

--- src/main/java/me/nokko/playeronlyloot/mixin/PlayerOnlyLootMixin.java
+++ src/main/java/me/nokko/playeronlyloot/mixin/PlayerOnlyLootMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,7 +21,7 @@ public abstract class PlayerOnlyLootMixin extends Entity {
 	@Inject(at = @At("HEAD"), method = "drop(Lnet/minecraft/entity/damage/DamageSource;)V", cancellable = true)
 	private void preventNonPlayerDrops(DamageSource damageSource, CallbackInfo ci) {
 		Entity attacker = damageSource.getAttacker();
-		if (attacker == null || !attacker.isPlayer()) {
+		if (!(attacker instanceof PlayerEntity)) {
 			ci.cancel();
 		}
 	}
